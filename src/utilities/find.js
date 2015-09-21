f76588ed4a915791ab4fdb216104b3cf194a782d
Roel van Uden <r.h.g.v.uden@outlook.com>
2015-09-21 16:00:43 +0200

    Document the required ignorePatterns (2.2.3)

--- src/utilities/find.js
+++ src/utilities/find.js
@@ -6,12 +6,14 @@ var glob = require('glob');
  * Finds the files matched by the patterns in the source directory.
  * @param {(Array.<string>|string)} patterns
  * @param {string} sourcePath
+ * @param {string} ignorePatterns
  * @param {function(Error, Array.<string>=)} done
  */
-module.exports = function(patterns, sourcePath, ignore, done) {
+module.exports = function(patterns, sourcePath, ignorePatterns, done) {
   var relativePaths = {};
   each(patterns, function(pattern, next) {
-    glob(pattern, {cwd: sourcePath, nodir: true, ignore: ignore}, function(err, foundPaths) {
+    console.log(pattern);
+    glob(pattern, {cwd: sourcePath, nodir: true, ignore: ignorePatterns}, function(err, foundPaths) {
       if (err) return next(err);
       foundPaths.forEach(function(foundPath) {
         relativePaths[foundPath] = true;
